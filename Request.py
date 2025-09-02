3bac29d2b9888100d1ea704ed29c7b8ec787c37a
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-02 21:50:15 +0530

    Update Request.py

--- Request.py
+++ Request.py
@@ -3,12 +3,14 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Route for GET request
+
 @app.route('/get_example', methods=['GET'])
 def get_example():
     name = request.args.get('name', 'Guest')  # Get parameter from URL
     return jsonify({"message": f"Hello, {name}! This is a GET request."})
 
 # Route for POST request
+
 @app.route('/post_example', methods=['POST'])
 def post_example():
     data = request.get_json()  # Get JSON data from request body
@@ -16,4 +18,5 @@ def post_example():
     return jsonify({"message": f"Hello, {name}! This is a POST request."})
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
